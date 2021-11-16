188e61426c605345cbbc5067bf995f6fc3d02846
Krystian Kozioł <krystian.koziol@gmail.com>
2021-11-16 15:54:31 +0100

    Seaborn plots

--- polts.pl.py
+++ polts.pl.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
-#https://scriptverse.academy/tutorials/python-matplotlib-plot.html
+
 
 #%% plot of temp
 mounths = [1,2,3,4,5,6,7,8,9,10,11,12]
